a670d6fe0af9b2a907b05e4c72b72303773120c0
zstiu <zstiu@foxmail.com>
2017-05-20 15:11:58 +0800

    初步侧边导航

---
+++ manager/src/components/notLogin.jsx
@@ -0,0 +1,41 @@
+import React, { PropTypes, Component } from 'react'
+import { Alert, Button, Icon } from 'antd';
+import { browserHistory } from 'react-router'
+// import { Link } from 'react-router'
+
+class NotLogin extends Component {
+
+    handelLogin = () => {
+      browserHistory.push(`/login`)
+  }
+
+
+  render(){
+    return (
+          <div>
+
+          <Alert
+            message="Warning"
+            description="你还未登录，登录前所有操作不能进行"
+            type="warning"
+            showIcon
+          />
+            
+            <Button onClick={this.handelLogin}>
+              <Icon type="login" />前往登录
+            </Button>
+
+            </div>
+        )
+      }
+  }
+
+// NotLogin.propTypes = {
+//   manager: PropTypes.shape({
+//     name: PropTypes.string.isRequired,
+//     managerId: PropTypes.string.isRequired,
+//     // name: PropTypes.string
+//   }).isRequired
+// }
+
+export default NotLogin
--- manager/src/containers/App.js
+++ manager/src/containers/App.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
 // import { connect } from 'react-redux'
 // import { browserHistory } from 'react-router'
-// import Explore from '../components/Explore'
+import NotLogin from '../components/notLogin'
+import LoginPage from './LoginPage'
 // import { resetErrorMessage } from '../actions'
 
 
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon, Button } from 'antd';
 import './dist/App.css'
 const { Header, Content, Footer,  Sider } = Layout;
 const SubMenu = Menu.SubMenu;
 
 class App extends Component {
+
+  static propTypes = {
+    manager: PropTypes.object,
+  }
+
+  handelChange = ({item, key, keyPath}) => {
+    console.log("点击了${key}")
+  }
+
+  handelLogin = () => {
+    console.log("进入login");
+      browserHistory.push(`/login`)
+  }
+
+  handelUpload =() => {
+    browserHistory.push('/upload')
+  }
+
+  handelManager =() => {
+    browserHistory.push('/manager')
+  }
+
+  handelUpload =() => {
+    browserHistory.push('/upload')
+  }
   
   state = {
     collapsed: false,
@@ -24,7 +52,7 @@ class App extends Component {
     });
   }
   render() {
-    const { children } = this.props
+    const { children, handelLogin, handelManager, handelUpload, manager } = this.props
     return (
       <Layout>
         <Sider
@@ -33,26 +61,26 @@ class App extends Component {
           onCollapse={this.onCollapse}
         >
           <div className="logo" />
-          <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={['6']}>
+          <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={['6']} onclick={this.handelChange}>
             <SubMenu
               key="sub1"
-              title={<span><Icon type="user" /><span className="nav-text">User</span></span>}
+              title={<span><Icon type="user" /><span className="nav-text">{manager.isLogin?manager.name:"Manager(未登录)"}</span></span>}
             >
-              <Menu.Item key="1">Tom</Menu.Item>
+              <Menu.Item key="1"> <Button onClick={this.handelLogin }>Login</Button>  </Menu.Item>
               <Menu.Item key="2">Bill</Menu.Item>
               <Menu.Item key="3">Alex</Menu.Item>
             </SubMenu>
             <SubMenu
               key="sub2"
-              title={<span><Icon type="team" /><span className="nav-text">Team</span></span>}
+              title={<span><Icon type="team" /><span className="nav-text">操作</span></span>}
             >
-              <Menu.Item key="4">Team 1</Menu.Item>
+              <Menu.Item key="4"><Button onClick={this.handelUpload}>上传图片</Button></Menu.Item>
               <Menu.Item key="5">Team 2</Menu.Item>
             </SubMenu>
             <Menu.Item key="6">
               <span>
                 <Icon type="file" />
-                <span className="nav-text">File</span>
+                <span className="nav-text"><Button onClick={this.handelManager}>账户</Button></span>
               </span>
             </Menu.Item>
           </Menu>
@@ -69,8 +97,8 @@ class App extends Component {
             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
 
 
-
-              {children}
+            {manager.isLogin?children:<div><NotLogin/> <LoginPage/></div>}
+              
             </div>
           </Content>
           <Footer style={{ textAlign: 'center' }}>
@@ -85,6 +113,27 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
 
 
-export default App
+  return {
+    manager: state.manager
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // handelChange: () => getManager("测试3", "123456")(dispatch)
+  }
+}
+
+// Action Creator
+// const increaseAction = { type: 'increase' }
+
+export default connect(
+    mapStateToProps, 
+    mapDispatchToProps
+)(App)
+
+
+// export default App
