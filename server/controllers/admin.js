1adf8a031d4d3fed991c788e1e2bbd4505dce845
zstiu <zstiu@foxmail.com>
2017-05-26 00:16:11 +0800

    后端精简项目

--- server/controllers/admin.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-    async indexPage(ctx) {
-        const title = 'admin page';
-        ctx.response.body = "首页";
-    },
-
-}
--- server/routers/index.js
+++ server/routers/index.js
@@ -4,18 +4,10 @@
 
 const router = require('koa-router')()
 
-const home = require('./home')
-const api = require('./api')
-const admin = require('./admin')
-const work = require('./work')
-const error = require('./error')
 
-router.use('/', home.routes(), home.allowedMethods())
+const api = require('./api')
+
+
 router.use('/api', api.routes(), api.allowedMethods())
-router.use('/admin', admin.routes(), admin.allowedMethods())
-router.use('/work', work.routes(), work.allowedMethods())
-router.use('/error', error.routes(), error.allowedMethods())
 
 module.exports = router
-
-
