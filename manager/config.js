cf7cd9a9f636d4a9b8cc5145c8c0bd00f39d9632
zstiu <zstiu@foxmail.com>
2017-06-18 22:49:01 +0800

    部署web页面

--- manager/config.js
+++ manager/config.js
@@ -1,8 +1,8 @@
-    let apiUrl;
+    // let needurl;
 
     // if (process.env.NODE_ENV === 'production') {
-    // apiUrl = "115.159.26.94:3001/api"; //开发时使用  第一个腾讯云
-    apiUrl = "118.89.158.83:3001/api"; //测试真是环境时使用  第二个腾讯云
+    // let needurl = "115.159.26.94:3001/api"; //开发时使用  第一个腾讯云
+    // apiUrl = "118.89.158.83:3001/api"; //测试真是环境时使用  第二个腾讯云
     // apiUrl = "114.115.143.207:3001/api";//生产时使用  华为云
     // } else {
     //     apiUrl = "localhost:3001/api"
@@ -10,7 +10,7 @@
 
     const config = {
 
-        apiUrl: apiUrl
+        apiUrl: "115.159.26.94:3001/api"
     }
 
     module.exports = config
